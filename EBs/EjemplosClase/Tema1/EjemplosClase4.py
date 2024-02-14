b917e601bf8ddca7b42b66bd3da87693258e7fee
iDeadtroll <javielproductor@gmail.com>
2024-02-14 18:52:30 +0100

    EB Tema1.3

---
+++ EBs/EjemplosClase/Tema1/EjemplosClase4.py
@@ -0,0 +1,11 @@
+#Programacion Funcional
+
+#Iteraciones y comprenhension de listas/diccionarios
+
+# Metodo 1
+lista = [(1, 2), (3, 7), (9, 5)]
+
+result = 0
+
+for t in lista:
+result = result + (t[0] * t[1])
