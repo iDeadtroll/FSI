5715e638a8387564482986b2a9dbb0511f15da2d
joni <joni@gmail.com>
2024-03-13 11:48:18 +0100

    TrabajoGrupo19: Problema1 V1.1

--- TrabajoGrupo19/P1/P0.py
+++ TrabajoGrupo19/P1/P0.py
@@ -2,25 +2,9 @@ datos = dict()
 datos[0] = "Jonathan"
 print(datos)
 
+for i in range(1,11):
+    print(i)
 
-# Usamos import csv para leer el dataset.
-import csv
-
-diccionario1 = dict()
-id_registros = []
-encabezado = []
-
-with open("/home/developer/proyectos/FSI/TrabajoGrupo19/P1/19_ucl_stats.csv", "r", encoding="utf-8") as archivo:
-    contenido = csv.reader(archivo, delimiter=",")
-    
-    encabezado = next(contenido)
-
-    # Empesamos a leer el contenido desde la segunda linea
-    next(contenido, None)
-    #   Recorrido para construir el diccionario y la lista de IDs de cada registro.
-    for linea in contenido:
-        valor = tuple(linea)
-        id_registros.append(valor[0])
-        clave = valor[0]
-        diccionario1[clave] = valor
+reg = int(input("Introduce el ID del registro: "))
+print(type(reg))
         
--- TrabajoGrupo19/P1/P1.py
+++ TrabajoGrupo19/P1/P1.py
@@ -5,13 +5,13 @@ diccionario1 = dict()
 id_registros = []
 encabezado = ()
 
+# TODO: probar a leer el dataset sin libreria "csv"
 with open("/home/developer/proyectos/FSI/TrabajoGrupo19/P1/19_ucl_stats.csv", "r", encoding="utf-8") as archivo:
     contenido = csv.reader(archivo, delimiter=",")
-    # Guardamos la primera linea en
+    # Guardamos la primera linea en "encabezado"
     encabezado = next(contenido)
 
     # Empesamos a leer el contenido desde la segunda linea
-    next(contenido, None)
     # Recorrido para construir el diccionario y la lista de IDs de cada registro.
     for linea in contenido:
         valor = tuple(linea)
@@ -22,7 +22,7 @@ with open("/home/developer/proyectos/FSI/TrabajoGrupo19/P1/19_ucl_stats.csv", "r
 
 def nuevo_registro():
     # Nuevo registro generado a partir de los campos de la tupla encabezado. 
-    # Tupla que hace referencia a los indices de la encabezado del dataset.
+    # Tupla que hace referencia a los indices del encabezado del dataset.
     print("Introduzca nuevo registro")
 
     # "nuevo_registro" contendrá el mismo numero de elementos que "encabezado"
@@ -34,44 +34,48 @@ def nuevo_registro():
         nuevo_registro.append(nuevo_elemento)
         i = i + 1
 
-    #   La nueva_tupla será el valor del nuevo item del diccionario.
+    # La nueva_tupla será el valor del nuevo item del diccionario.
     nueva_tupla = tuple(nuevo_registro)
-    # print(nueva_tupla)
 
-    diccionario2 = dict()
-    diccionario2[(nueva_tupla[0])] = nueva_tupla
-    diccionario1.update(diccionario2)
-    print(diccionario1)
+    # TODO: Probar este misma funcion usan la funcion get() y la opcion por defecto.
+    # Evaluar si el nuevo registro existe, caso contrario se agrega el nuevo registro al diccionario.
+    if nueva_tupla[0] not in diccionario1:
+        diccionario1[(nueva_tupla[0])] = nueva_tupla
+        print(diccionario1.get(nueva_tupla[0]))
+    else:
+        print("El registro ", nueva_tupla[0], " ya existe. Intente con otro numero de registro.")
 
-    # TODO
-    #   Evaluar si el nuevo registro existe, caso contrario se agrega el nuevo registro al diccionario.
+
 
 
 def tabla_diccionario():
-
+    # TODO: mostrar un formato de tabla con los tamanios de los campos fijos
     for k, v in diccionario1.items():
         print("    |   ".join(map(str, v)))
 
 
 def borrar_registro():
+    # Mostramos los 10 primeros
+    for i in range(1,10):
+        print(diccionario1.get(str(i)))
+    # TODO: vefificar que el registro existe y pedir confirmacion para eliminarlo
 
-    print(diccionario1)
-    print("Clave formada por una tupla (prime indice, segundo indice)")
-    a = input("Introduce el primer indice de la tupla")
-    b = input("Introduce el segundo indice de la tupla")
-    t = (a, b)
-    diccionario1.pop(t)
-    print(diccionario1)
+    reg = str(input("Introduce el ID del registro: "))
+    diccionario1.pop(reg)
+
+    # TODO: mostrar el ID del registro que se ha aliminado e indicar que se ha realizado correctamente.
+
+    # Mostramos los 10 primeros y verificamos que se ha eliminado
+    for i in range(1,10):
+        print(diccionario1.get(str(i)))
 
 
 def busca_clave_mostrar_valor():
 
-    print("Clave formada por una tupla (prime indice, segundo indice)")
-    a = input("Introduce el primer indice de la tupla")
-    b = input("Introduce el segundo indice de la tupla")
-    t = (a, b)
-    valor = diccionario1.get(t)
-    print(valor)
+    # TODO: vefificar que el registro existe, caso contrario pedir nuevamente el ID
+    reg = str(input("Introduce el ID del registro: "))
+    
+    print(diccionario1.get(reg))
 
 
 def mostrarOpciones():
@@ -83,10 +87,11 @@ def mostrarOpciones():
     print("5. Salir")
 
 
+
 # Menu que muestra las opciones y evalua la opcion seleccionada
 s = True
 while s:
-    print(mostrarOpciones())
+    mostrarOpciones()
     opt = input("Seleccione una opcion (1-5): ")
     print("\n")
 
@@ -95,10 +100,7 @@ while s:
             print("Opcion: Agregar nuevo registro", end="\n\n")
             nuevo_registro()
         case "2":
-            print(
-                "Opcion: Buscar un registro por su clave y mostrar sus valores",
-                end="\n\n",
-            )
+            print("Opcion: Buscar un registro por su clave y mostrar sus valores", end="\n\n")
             busca_clave_mostrar_valor()
         case "3":
             print("Borrar un registro a partir de su clave", end="\n\n")
