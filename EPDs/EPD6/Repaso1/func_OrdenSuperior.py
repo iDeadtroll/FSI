ea1668e157cb88a922e80f2f3079e05c3145df6b
iDeadtroll <javielproductor@gmail.com>
2024-04-03 18:34:45 +0200

    Repaso1: func_OrdenSuperior V1

--- EPDs/EPD6/Repaso1/func_OrdenSuperior.py
+++ EPDs/EPD6/Repaso1/func_OrdenSuperior.py
@@ -10,3 +10,33 @@ print(l3)
 # Lista por comprension que desempaqueta l_aux y devuelve el producto de los elementos de cada tupla, si 'num' es mayor que 0
 l4 =[c * num for c, num in l_aux if num > 0]
 print(l4)
+
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Cuadrados de una lista de números: Dada una lista de números, escribe un programa que devuelva una nueva lista que contenga el cuadrado de cada número.
+lista_cuadrados = [n*n for n in numeros]
+print("Lista cuadrados: \n" , lista_cuadrados)
+# Filtrar números pares de una lista: Dada una lista de números, escribe un programa que devuelva una nueva lista que contenga solo los números pares.
+lista_pares = [n for n in numeros if n % 2 == 0]
+print(lista_pares)
+
+
+palabras = ['hola', 'mundo', 'python', 'comprension', 'de', 'listas']
+# Convertir una lista de palabras a mayúsculas: Dada una lista de palabras, escribe un programa que devuelva una nueva lista que contenga todas las palabras convertidas a mayúsculas.
+lista_mayusculas = [ n.upper() for n in palabras]
+print(lista_mayusculas)
+
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = ['a', 'b', 'c', 'd', 'e']
+# Crear una lista de tuplas a partir de dos listas: Dadas dos listas de igual longitud, escribe
+# un programa que devuelva una nueva lista que contenga tuplas. Cada tupla debe contener un elemento de
+# cada una de las listas originales que ocupan la misma posición.
+
+lista_tuplas = [(c,n) for c,n in zip(lista1,lista2)]
+print(lista_tuplas)
+
+map()
+
+
