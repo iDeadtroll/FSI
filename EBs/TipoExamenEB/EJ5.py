e0e79133434c13f368bf4529d3081304b91c57f6
Joni <joni@example.com>
2024-04-26 12:42:43 +0200

    TipoExamenEB: Ejercicios resueltos (1-6) + enunciados

--- EBs/TipoExamenEB/EJ5.py
+++ EBs/TipoExamenEB/EJ5.py
@@ -1,7 +1,25 @@
 # Escriba una clase que contenga tres propiedades: salario bruto, salario neto e IRPF. Suponga que el salario
-# neto es la diferencia entre el salario bruto y el IRPF(suponga tambien que el valor de IRFP introducido por 
+# neto es la diferencia entre el salario bruto y el IRPF(suponga tambien que el valor del IRFP introducido por 
 # el usuario es positivo). Programe un constructor para la clase que reciba el salario bruto y el IRPF(el 
 # salario neto se calcula en dicho constructor). Programe un método modificador para la propiedad del salario 
 # bruto de manera que no se permita almacenar dicho salario negativo o si el valor es inferior al de la
 # propiedad IRPF. En tal caso, escribir un mensaje al usuario indicando tal error. Además, dicho modificador
 # deberá actualizar el valor de la propiedad del salario neto convenientemente. No se pide ningun otro metodo.
+
+class Empleo:
+
+    def __init__(self, salarioBruto : float, IRPF: float):
+        self.__salarioBruto = salarioBruto
+        self.__irpf = IRPF
+        self.__salarioNeto = salarioBruto - IRPF
+
+
+    def salarioBruto(self, sBruto):
+        if sBruto < 0 or sBruto < self.__irpf:
+            print("Error: el salario bruto no debe ser menor que 0 ó menor IRPF")
+
+        else:
+            self.__salarioBruto = sBruto
+            self.__salarioNeto = self.__salarioBruto - self.__irpf
+
+
